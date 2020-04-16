cdeceae214e56681fc72f09d3a684bbd2aadb076
Alec Dye <alecdye@gmail.com>
2020-04-16 09:57:30 -0700

    Answering queries Q&A

--- queries.sql
+++ queries.sql
@@ -3,10 +3,23 @@
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 SELECT ProductName, CategoryName FROM Product
 JOIN Category
-ON Product.CategoryId = Category.Id;
+on Product.CategoryId = Category.Id;
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+SELECT [Order].Id, Shipper.CompanyName
+FROM [Order]
+JOIN Shipper on [Order].ShipVia = Shipper.Id
+WHERE [Order].OrderDate < date("2012-08-09");
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+SELECT Product.ProductName, Product.Id, OrderDetail.Quantity
+FROM OrderDetail
+JOIN Product on Product.Id = OrderDetail.ProductId
+JOIN [Order] on [Order].Id = OrderDetail.OrderId
+WHERE [Order].Id = 10251;
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+SELECT [Order].Id, Customer.CompanyName, Employee.LastName
+FROM [Order]
+JOIN Customer on Customer.Id = [Order].CustomerId
+JOIN Employee on Employee.Id = [Order].EmployeeId;
