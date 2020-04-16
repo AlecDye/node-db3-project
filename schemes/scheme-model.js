efe71eee34b1db8a0c61c20f1ebc597b413bad0c
Alec Dye <alecdye@gmail.com>
2020-04-15 17:22:11 -0700

    Building db config file

---
+++ schemes/scheme-model.js
@@ -0,0 +1,21 @@
+const db = require("../data/db-config.js");
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  addStep,
+  update,
+  remove,
+};
+
+function find() {}
+
+function findById() {}
+
+function findSteps() {}
+
+function add() {}
+
+function remove() {}
